580d21f889b24657738248ea2a5b8df0b410a852
Rithvik <rithvikvit@gmail.com>
2025-03-08 14:26:02 +0530

    Commit all

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,31 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import StopWatch from "./StopWatch";
 
-function StopWatch() {
-  const [time, setTime] = useState(0);
-  const [isRunning, setIsRunning] = useState(false);
-
-  useEffect(() => {
-    if (isRunning) {
-      const interval = setInterval(() => setTime((prevTime) => prevTime + 1), 1000);
-      return () => clearInterval(interval);
-    }
-  }, [isRunning]);
-
-  const formatTime = () => {
-    const minutes = Math.floor(time / 60);
-    const seconds = time % 60;
-    return `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
-  };
-
+function App() {
   return (
     <div>
-      <h2>Stopwatch</h2>
-      <h1>{formatTime()}</h1>
-      <button onClick={() => setIsRunning(true)}>Start</button>
-      <button onClick={() => setIsRunning(false)}>Stop</button>
-      <button onClick={() => { setTime(0); setIsRunning(false); }}>Reset</button>
+      <h1>Stopwatch Example</h1>
+      <StopWatch />
     </div>
   );
 }
 
-export default StopWatch;
+export default App;
